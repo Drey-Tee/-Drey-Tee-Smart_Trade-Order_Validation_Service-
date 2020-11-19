7dc5202e6f9d3b59bd7ac8277590bd3459448112
DummyCOd3r__ <jameaamo@gmail.com>
2020-11-19 20:30:06 +0000

    added suppressed response to client connectivity

--- src/main/java/com/soapcon/springbootwithsoap/endpoint/UserEndpoint.java
+++ src/main/java/com/soapcon/springbootwithsoap/endpoint/UserEndpoint.java
@@ -9,9 +9,6 @@ import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Endpoint
 public class UserEndpoint {
 //    private GetUserResponse responses;
@@ -22,21 +19,15 @@ public class UserEndpoint {
     @PayloadRoot(namespace = "http://soapspringboot.com/springboot",
     localPart = "getUserRequest") // define namespace to retrieve the user data/ spring web service identifies
     @ResponsePayload //convert payload to soap payload type
-    public GetUserResponse getUserRequest(@RequestPayload GetUserRequest request){
+    public GetUserResponse getUserRequest(@RequestPayload GetUserRequest request) {
         GetUserResponse response = new GetUserResponse();
         response.setUser(validationService.getUsers(request.getId())); //from request, get the id
-
-        List<String> message = new ArrayList<>();
-        message.add("Success");
-        message.add("Fail");
-
-        if (request.getId() == response.getUser().getId()){
-            System.out.println("Connect successfully");
+        if (response.getUser() != null) {
+            System.out.println("Successful Response");
         }
         else {
-            System.out.println("Failed to connect");
+            System.out.println("Failed Response");
         }
         return null;
-
     }
 }
