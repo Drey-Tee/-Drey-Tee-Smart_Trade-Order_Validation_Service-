3b1a6aba1eaa12edd7e40a69c350a4927c1eb097
DummyCOd3r__ <jameaamo@gmail.com>
2020-11-19 16:37:33 +0000

    changed response object to  null

--- src/main/java/com/soapcon/springbootwithsoap/endpoint/UserEndpoint.java
+++ src/main/java/com/soapcon/springbootwithsoap/endpoint/UserEndpoint.java
@@ -4,14 +4,17 @@ import com.soapcon.springbootwithsoap.service.ValidationService;
 import com.soapspringboot.springboot.GetUserRequest;
 import com.soapspringboot.springboot.GetUserResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Endpoint
 public class UserEndpoint {
+//    private GetUserResponse responses;
 
     @Autowired
     public ValidationService validationService; //autowiring the service create (ValidationService)
@@ -22,6 +25,18 @@ public class UserEndpoint {
     public GetUserResponse getUserRequest(@RequestPayload GetUserRequest request){
         GetUserResponse response = new GetUserResponse();
         response.setUser(validationService.getUsers(request.getId())); //from request, get the id
-        return response;
+
+        List<String> message = new ArrayList<>();
+        message.add("Success");
+        message.add("Fail");
+
+        if (request.getId() == response.getUser().getId()){
+            System.out.println("Connect successfully");
+        }
+        else {
+            System.out.println("Failed to connect");
+        }
+        return null;
+
     }
 }
